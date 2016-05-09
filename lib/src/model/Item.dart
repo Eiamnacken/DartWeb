8a7a0ca8de2e6afb615b3b5aa4528a9a13015dc4
Sven Marquardt <sven2q@hotmail.de>
2016-05-09 08:12:19 +0200

    Projekt struktur neu definiert 	neue Datei:     lib/BrickGame.dart 	gelöscht:       lib/model/Model.dart 	umbenannt:      lib/model/GameController.dart -> lib/src/controller/GameController.dart 	neue Datei:     lib/src/model/Ball.dart 	neue Datei:     lib/src/model/Brick.dart 	neue Datei:     lib/src/model/Enums.dart 	umbenannt:      lib/model/Game.dart -> lib/src/model/Game.dart 	neue Datei:     lib/src/model/GameObject.dart 	neue Datei:     lib/src/model/Item.dart 	neue Datei:     lib/src/model/Level.dart 	neue Datei:     lib/src/model/Model.dart 	neue Datei:     lib/src/model/Player.dart 	geändert:       bin/doc/DartWeb.asta 	geändert:       lib/src/controller/GameController.dart 	geändert:       lib/src/model/Model.dart 	bin/doc/api/Game/Game/countLevel.html 	bin/doc/api/Game/Game/moveBall.html 	bin/doc/api/Game/Game/moveItem.html 	bin/doc/api/Game/Game/newLevel.html 	bin/doc/api/GameController/ 	bin/doc/api/Model/Ball/collision.html 	bin/doc/api/Model/GameObject-class.html 	bin/doc/api/Model/GameObject/ 	bin/doc/api/Model/Item/collision.html 	bin/doc/api/Model/Level-class.html 	bin/doc/api/Model/Level/ 	bin/doc/api/Model/MoveableObject/collision.html 	bin/doc/api/Model/MoveableObject/collisionAhead.html 	bin/doc/api/Model/Player/collision.html 	packages/

---
+++ lib/src/model/Item.dart
@@ -0,0 +1,37 @@
+import 'package:DartWeb/model/GameObject.dart';
+import 'package:DartWeb/model/Enums.dart';
+import 'package:DartWeb/model/Player.dart';
+
+///
+/// [Item] sind die Positiven oder Negativen effekte die der Player im laufe des
+/// Spieles einsammeln kann
+///
+class Item extends MoveableObject {
+  ///
+  /// Handelt es sich um ein Negativ[Effect] oder Positiv[Effect]
+  ///
+  bool _isPositive;
+
+  ///
+  /// Um welche art von [Item] handelt es sich
+  ///
+  Effect effect;
+
+  ///
+  /// Gibt an ob es ein Positiv Effekt ist
+  ///
+  bool isPositive() {
+    //TODO: Implement Method
+  }
+
+  @override
+  void collision(Direction direction, List<List<GameObject>> gameField) {
+    // TODO: implement collision
+  }
+  ///
+  /// Wendet die eigenschaften dieses Items auf den Player an
+  ///
+  void _activateItem(Player player){
+    //TODO: Implement Method
+  }
+}
